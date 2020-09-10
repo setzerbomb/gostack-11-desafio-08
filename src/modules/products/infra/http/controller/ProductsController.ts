07c545363e367e84d50a40aa24ccb9bc101ec7d8
Set <6942774+setzerbomb@users.noreply.github.com>
2020-09-09 22:01:11 -0300

    desafio completo

---
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+import CreateProductService from '@modules/products/services/CreateProductService';
+
+export default class ProductsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    // TODO
+    try {
+      const { name, price, quantity } = request.body;
+
+      const createProduct = container.resolve(CreateProductService);
+
+      const product = await createProduct.execute({
+        name,
+        price,
+        quantity,
+      });
+
+      return response.json(product);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+}
